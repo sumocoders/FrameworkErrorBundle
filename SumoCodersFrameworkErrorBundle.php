fc6af97f4f4f99d49049aad6af726b3036973bd4
Tijs Verkoyen <tijs@verkoyen.eu>
2014-01-26 19:47:53 +0100

    Tell Symfony our parent is the TwigBundle, so we can overrule the errorpages

--- SumoCodersFrameworkErrorBundle.php
+++ SumoCodersFrameworkErrorBundle.php
@@ -6,4 +6,11 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SumoCodersFrameworkErrorBundle extends Bundle
 {
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'TwigBundle';
+    }
 }
